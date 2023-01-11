28e0281692694c3b3da48a9468e03cdb850a722a
Alex Bowler <albowler@umich.edu>
2023-01-11 14:44:34 -0500

    Initial Commit. Starter files with function stubs.

---
+++ resize.cpp
@@ -0,0 +1,10 @@
+//Image Resizing Project
+// Project UID af1f95f547e44c8ea88730dfb185559d
+//By Alex Bowler
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    cout<<"Hello World"<<endl;
+}
